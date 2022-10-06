1d82d3d893e1512ce6ff2e8089a9873cb826a3ed
“Reshma <3areshma@email.com>
2022-10-05 19:09:04 -0500

    part 2

---
+++ starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -0,0 +1,44 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
+public class MovieController {
+    @Autowired
+    BestMovieService bestMovieService;
+
+    // need a controller method to show the initial HTML form
+
+    public String getIndexPage(){
+        return "index";
+    }
+
+
+    // need a controller method to process the HTML form
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage(Model model) {
+        model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle());
+        return "bestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovieForm")
+    public String voteForBestMovieFormPage() {
+        return "voteForBestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovie")
+    public String voteForBestMovie(HttpServletRequest request, Model model) {
+
+        String movieTitle = request.getParameter("movieTitle");
+
+        model.addAttribute("BestMovieVote", movieTitle);
+
+        return "voteForBestMovie";
+    }
+}
